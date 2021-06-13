ba75a7dc57c4e8b75eb836d760f53b5f56750378
hajkati <79836425+hajkati@users.noreply.github.com>
2021-06-13 17:49:04 +0100

    committtttsss

--- src/app/view/Prof/schedule/schedule.component.ts
+++ src/app/view/Prof/schedule/schedule.component.ts
@@ -7,9 +7,9 @@ import {ScheduleProf} from '../../../controller/model/calendrier-prof.model';
 import {ScheduleService} from '../../../controller/service/schedule.service';
 import {ConfirmationService, MessageService} from 'primeng/api';
 import {ScheduleVo} from '../../../controller/model/schedule-vo.model';
-import {EtatInscription} from "../../../controller/Model/etat-inscription.model";
-import {Etudiant} from "../../../controller/Model/etudiant.model";
-import {EtatEtudiantSchedule} from "../../../controller/Model/etat-etudiant-schedule.model";
+import {EtatInscription} from '../../../controller/Model/etat-inscription.model';
+import {Etudiant} from '../../../controller/Model/etudiant.model';
+import {EtatEtudiantSchedule} from '../../../controller/Model/etat-etudiant-schedule.model';
 import {EtudiantService} from '../../../controller/service/etudiant.service';
 import {LoginService} from '../../../controller/service/login.service';
 import {Prof} from '../../../controller/Model/prof.model';
@@ -135,6 +135,7 @@ export class ScheduleComponent implements OnInit {
   set etudiant(value: Etudiant) {
     this.service.etudiant = value;
   }
+  // tslint:disable-next-line:adjacent-overload-signatures
   set itemsVo(value: Array<ScheduleVo>) {
     this.service.itemsVo = value;
   }
@@ -226,7 +227,7 @@ export class ScheduleComponent implements OnInit {
     console.log('etudiant' + this.selected.etudiant.id);
     console.log(this.selected.dateDebut);
     console.log(this.selected.dateFin);
-      this.service.addStudent().subscribe(data => {
+    this.service.addStudent().subscribe(data => {
         this.items.push({...data});
         this.messageService.add({
           severity: 'success',
@@ -235,8 +236,8 @@ export class ScheduleComponent implements OnInit {
           life: 3000
         });
       });
-      this.createDialog = false;
-      this.selected = new ScheduleProf();
+    this.createDialog = false;
+    this.selected = new ScheduleProf();
     }
 }
 
