e11de7d824e0aa61267869f16845466318fc3217
mohcinemourchid99 <mohcine.mourchid@edu.uca.ma>
2021-06-24 14:03:51 +0100

    faq

--- src/app/view/admin/faq-admin/faq-answer/faq-answer.component.ts
+++ src/app/view/admin/faq-admin/faq-answer/faq-answer.component.ts
@@ -5,6 +5,7 @@ import {FaqProf} from '../../../../controller/Model/faq-prof.model';
 import {FaqType} from '../../../../controller/Model/faq-type.model';
 import {Faq} from '../../../../controller/Model/faq.model';
 import {LoginService} from '../../../../controller/service/login.service';
+import {FaqEtudiant} from '../../../../controller/Model/faq-etudiant.modele';
 
 @Component({
   selector: 'app-faq-answer',
@@ -17,6 +18,8 @@ export class FaqAnswerComponent implements OnInit {
   answer: string;
   nodes: TreeNode[];
   menu: MenuItem[];
+  utilisateur: string;
+  by: string;
 
   constructor(private service: FaqService, private login: LoginService) { }
 
@@ -75,49 +78,147 @@ export class FaqAnswerComponent implements OnInit {
   set itemsType(value: Array<FaqType>) {
     this.service.itemsType = value;
   }
+
+  get selectedFaqEtudiant(): FaqEtudiant {
+    return this.service.selectedFaqEtudiant;
+  }
+
+  set selectedFaqEtudiant(value: FaqEtudiant) {
+    this.service.selectedFaqEtudiant = value;
+  }
   public update(libelle: string)
   {
-    this.service.findFaqProfByLibelle(libelle).subscribe(
-        data =>{
-          this.selectedFaqProf = data;
-          console.log(this.selectedFaqProf);
-          this.selectedFaqProf.description = this.answer;
-          this.selectedFaqProf.admin = this.login.admin;
-          this.service.updateFaqrof(this.selectedFaqProf).subscribe(
-              data => {
-              },error => {
-                console.log('erreur');
-              }
-          );
-        }
-    );
+    if(this.utilisateur == 'teacher')
+    {
+      this.service.findFaqProfByLibelle(libelle).subscribe(
+          data =>{
+            this.selectedFaqProf = data;
+            console.log(this.selectedFaqProf);
+            this.selectedFaqProf.description = this.answer;
+            this.selectedFaqProf.admin = this.login.admin;
+            this.service.updateFaqrof(this.selectedFaqProf).subscribe(
+                data => {
+                },error => {
+                  console.log('erreur');
+                }
+            );
+          }
+      );
+    }
+    else if(this.utilisateur == 'student')
+    {
+      this.service.findFaqEtudiantByLibelle(libelle).subscribe(
+          data =>{
+            this.selectedFaqEtudiant = data;
+            this.selectedFaqEtudiant.description = this.answer;
+            this.selectedFaqEtudiant.admin = this.login.admin;
+            console.log(this.selectedFaqEtudiant);
+            this.service.updateFaqEtudiant(this.selectedFaqEtudiant).subscribe(
+                data => {
+                },error => {
+                  console.log('erreur');
+                }
+            );
+          }
+      );
+    }
 
   }
   public init()
   {
+    this.answer ='';
+    if(this.utilisateur == 'teacher')
+    {
+      this.service.findFaqProf().subscribe(data => {
+        this.items = data;
+        this.nodes = [];
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0 ; i < this.items.length ; i++)
+        {
+          this.nodes.push(
+              {
+                label: this.items[i].libelle, key: this.items[i].libelle,
+                children: [
+                  {label: this.items[i].description, type: 'string'}
+                ]
+              }
+          );
+        }
+      });
+    }
+    else if(this.utilisateur == 'student')
+    {
+      this.service.findFaqEtudiant().subscribe(data => {
+        this.items = data;
+        this.nodes = [];
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0 ; i < this.items.length ; i++)
+        {
+          this.nodes.push(
+              {
+                label: this.items[i].libelle, key: this.items[i].libelle,
+                children: [
+                  {label: this.items[i].description, type: 'string'}
+                ]
+              }
+          );
+        }
+      });
+    }
+  }
+  public initUtilisateur()
+  {
     this.menu = [
-      {label: 'Teacher'},
-      {label: 'Student'},
+      {label: 'Teacher', command: (event) => {
+          this.utilisateur = 'teacher';
+        }},
+      {label: 'Student', command: (event) => {
+          this.utilisateur = 'student';
+        }},
     ];
-    this.service.findFaqProf().subscribe(data => {
-      this.items = data;
-      this.nodes = [];
-      // tslint:disable-next-line:prefer-for-of
-      for (let i = 0 ; i < this.items.length ; i++)
-      {
-        this.nodes.push(
-            {
-              label: this.items[i].libelle, key: this.items[i].libelle,
-              children: [
-                {label: this.items[i].description, type: 'string'}
-              ]
-            }
-        );
-      }
-    });
+
+    this.utilisateur = 'teacher';
+    if(this.utilisateur == 'teacher')
+    {
+      this.service.findFaqProf().subscribe(data => {
+        this.items = data;
+        this.nodes = [];
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0 ; i < this.items.length ; i++)
+        {
+          this.nodes.push(
+              {
+                label: this.items[i].libelle, key: this.items[i].libelle,
+                children: [
+                  {label: this.items[i].description, type: 'string'}
+                ]
+              }
+          );
+        }
+      });
+    }
+    else if(this.utilisateur == 'student')
+    {
+      this.service.findFaqEtudiant().subscribe(data => {
+        this.items = data;
+        this.nodes = [];
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0 ; i < this.items.length ; i++)
+        {
+          this.nodes.push(
+              {
+                label: this.items[i].libelle, key: this.items[i].libelle,
+                children: [
+                  {label: this.items[i].description, type: 'string'}
+                ]
+              }
+          );
+        }
+      });
+    }
   }
   ngOnInit(): void {
-    this.init();
+    this.initUtilisateur();
   }
 
 }
